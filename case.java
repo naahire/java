36be5f23da532f1b74244e76b605922243f3c2b3
naahire <65179560+naahire@users.noreply.github.com>
2020-08-23 00:30:20 +0530

    Add files via upload

---
+++ case.java
@@ -0,0 +1,9 @@
+class string
+{
+public static void main(String args[])
+	{
+		String s="Nilesh Ahire";
+		System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());	
+	}	
+}
