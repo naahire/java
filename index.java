96d27d3e16110b2d8d69660d372ca561dc79569d
naahire <65179560+naahire@users.noreply.github.com>
2020-08-23 01:13:45 +0530

    Add files via upload

---
+++ index.java
@@ -0,0 +1,9 @@
+public class index
+{
+public static void main(String args[])
+	{
+		String s="Nilesh Ashok Ahire study in P.V.P. clg Loni";
+		System.out.println(s.indexOf("Ashok"));	
+		\\Denote the word or alphabet which no of position in string 
+	}	
+}
