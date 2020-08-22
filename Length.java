ba54efab2c3d894fa9e68beb666738166288fbb6
naahire <65179560+naahire@users.noreply.github.com>
2020-08-23 00:35:34 +0530

    Add files via upload

---
+++ Length.java
@@ -0,0 +1,9 @@
+public class string
+{
+public static void main(String args[])
+	{
+		String s="Nilesh Ahire";
+		System.out.println(s.length());	
+        //length() calculate length of the string
+	}	
+}
