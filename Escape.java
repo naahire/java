0b198bf3c9740b9d40502985da612e3012b0d4a0
naahire <65179560+naahire@users.noreply.github.com>
2020-08-23 23:52:19 +0530

    Add files via upload

---
+++ Escape.java
@@ -0,0 +1,14 @@
+public class myclas
+{
+public static void main(String args[])
+{
+System.out.println("My name is \nNilesh Ahire ");
+System.out.println("My name is \' Nilesh Ahire ");
+System.out.println("My name is \" Nilesh Ahire ");
+System.out.println("My name is \\ Nilesh Ahire ");
+System.out.println("My name is \rNilesh Ahire ");
+System.out.println("My name is \tNilesh Ahire ");
+System.out.println("My name is \bNilesh Ahire ");
+System.out.println("My name is \fNilesh Ahire ");
+}
+}
