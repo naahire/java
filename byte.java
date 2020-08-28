6823d29e622bbfa9a1da482e1d039a834bdad8ff
naahire <65179560+naahire@users.noreply.github.com>
2020-08-28 23:06:38 +0530

    Add files via upload

---
+++ byte.java
@@ -0,0 +1,22 @@
+public class Byte{
+  public static void main(String[] args){
+  byte b=72;
+  int i=-43;
+  float f=56.78f;
+  char c='N';
+  String s="Nilesh";
+  Short S=32766;
+  long l=92345688990887L;
+  double d=67788886655;
+  boolean B=true;
+  System.out.println("byte ="+b);
+  System.out.println("int ="+i);
+  System.out.println("flote ="+f);
+System.out.println("char ="+c);
+System.out.println("String ="+s);
+System.out.println("Short ="+S);
+System.out.println("long ="+l);
+System.out.println("double ="+d);
+System.out.println("boolean ="+B);
+  }
+}
