05a7ce9ec738680f38143ba610c0a2ea5fb89ebb
naahire <65179560+naahire@users.noreply.github.com>
2020-09-03 00:02:00 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,9 @@
+public class Ifelse{
+  public static void main(String[] args){
+    int a=10,b=17;
+   if(a>b)
+   System.out.println(a+" is greatee");
+   else
+   System.out.println(b+" is greatee");
+  }
+}
