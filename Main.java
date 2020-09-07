3590708ed975b97fd39efffd7ec3a31adbee598d
naahire <65179560+naahire@users.noreply.github.com>
2020-09-07 23:29:45 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+class positive{
+int x=5;
+}
+class negative{
+int x=-7;
+}
+public class Main{
+public static void main(String[] args){
+positive p=new positive();
+negative n=new negative();
+System.out.println(p.x);
+System.out.println(n.x);
+}
+}
